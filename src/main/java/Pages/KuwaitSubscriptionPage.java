1e2ba4ce15d17264f91034eeea8bc10cbf128184
Aya Mohamed <ayamohamedmahmoud@gmail.com>
2024-02-19 19:54:03 +0300

    Create KuwaitSubscriptionPage class

---
+++ src/main/java/Pages/KuwaitSubscriptionPage.java
@@ -0,0 +1,13 @@
+package Pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class KuwaitSubscriptionPage {
+
+    WebDriver driver;
+    public KuwaitSubscriptionPage(WebDriver driver)
+    {
+        this.driver=driver;
+    }
+
+}
